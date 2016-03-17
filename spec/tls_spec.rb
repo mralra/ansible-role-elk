2f1156840713b5c3f0e782b6b8b23d593d928ecb
Conor Schaefer <conor.schaefer@gmail.com>
2016-03-17 16:59:14 -0700

    Disables all SSL checks in Serverspec by default
    The test VM doesn't have SSL keys, so can't test them.
    The tests are still present and can be enabled via an env var when
    running the tests, for example if you wish to verify a staging or prod
    config remotely.

--- spec/tls_spec.rb
+++ spec/tls_spec.rb
@@ -1,38 +1,38 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe file('/etc/nginx/conf.d/kibana.conf') do
-  it { should be_file }
-  its('content') { should match(%r{^\s+ssl_certificate \/[^\0]+;}) }
-  its('content') { should match(%r{^\s+ssl_certificate_key \/[^\0]+;}) }
+# TODO: Disabling the SSL config checks because the tests don't use a
+# self-signed cert, since there aren't prod SSL certs available for testing.
+if property['serverspec_elk_test_ssl']
+  describe file('/etc/nginx/conf.d/kibana.conf') do
+    it { should be_file }
+    its('content') { should match(%r{^\s+ssl_certificate \/[^\0]+;}) }
+    its('content') { should match(%r{^\s+ssl_certificate_key \/[^\0]+;}) }
 
-  desired_ssl_config_lines = [
-    'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;',
-    'ssl_prefer_server_ciphers on;',
-    'ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";',
-    'ssl_ecdh_curve secp384r1;',
-    'ssl_session_cache shared:SSL:10m;',
-    'ssl_session_tickets off;',
-    'ssl_stapling on;',
-    'ssl_stapling_verify on;',
-    'resolver 127.0.0.1 valid=300s;',
-    'resolver_timeout 5s;',
-    'add_header Strict-Transport-Security \
-    "max-age=63072000; includeSubdomains; preload";',
-    'add_header X-Frame-Options DENY;',
-    'add_header X-Content-Type-Options nosniff;'
-  ]
-  # TODO: Disabling the SSL config checks because the tests don't use a
-  # self-signed cert, since there aren't prod SSL certs available for testing.
-  if property['serverspec_elk_test_ssl']
+    desired_ssl_config_lines = [
+      'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;',
+      'ssl_prefer_server_ciphers on;',
+      'ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";',
+      'ssl_ecdh_curve secp384r1;',
+      'ssl_session_cache shared:SSL:10m;',
+      'ssl_session_tickets off;',
+      'ssl_stapling on;',
+      'ssl_stapling_verify on;',
+      'resolver 127.0.0.1 valid=300s;',
+      'resolver_timeout 5s;',
+      'add_header Strict-Transport-Security \
+      "max-age=63072000; includeSubdomains; preload";',
+      'add_header X-Frame-Options DENY;',
+      'add_header X-Content-Type-Options nosniff;'
+    ]
     desired_ssl_config_lines.each do |ssl_config_line|
       its('content') { should match(/#{Regexp.quote(ssl_config_line)}/) }
     end
   end
-end
 
-describe port(443) do
-  it { should be_listening.on('0.0.0.0') }
-  it { should be_listening.with('tcp') }
-  it { should_not be_listening.on('127.0.0.1') }
+  describe port(443) do
+    it { should be_listening.on('0.0.0.0') }
+    it { should be_listening.with('tcp') }
+    it { should_not be_listening.on('127.0.0.1') }
+  end
 end
