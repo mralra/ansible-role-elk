00888c50ffcbcda5a6491d9e7e0d7893a009328a
Conor Schaefer <conor@freedom.press>
2016-04-24 00:03:53 -0700

    Adds required module structure to filter plugins

--- filter_plugins/kibana_dashboards.py
+++ filter_plugins/kibana_dashboards.py
@@ -5,9 +5,18 @@ def kibana_dashboards(kibana_json):
     return [x for x in kibana_json if x['_type'] == 'dashboard']
 
 
+def kibana_searches(kibana_json):
+    return [x for x in kibana_json if x['_type'] == 'search']
+
+
 def kibana_visualizations(kibana_json):
     return [x for x in kibana_json if x['_type'] == 'visualization']
 
 
-def kibana_searches(kibana_json):
-    return [x for x in kibana_json if x['_type'] == 'search']
+class FilterModule(object):
+    def filters(self):
+        return {
+            'kibana_dashboards': kibana_dashboards,
+            'kibana_searches': kibana_searches,
+            'kibana_visualizations': kibana_visualizations,
+        }
