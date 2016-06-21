ff3931882219fa1aa0c8b1b1586642c9625d821b
Conor Schaefer <conor@freedom.press>
2016-06-21 15:53:00 -0700

    Updates kibana tests to with apt repo config
    Checks that /opt/kibana is a directory, rather than a symlink, along
    with proper ownership and permissions. Also checks the apt config for
    the repo itself, and ensures the package is installed at version 4.5.1
    or greater.

--- spec/kibana_spec.rb
+++ spec/kibana_spec.rb
@@ -3,13 +3,31 @@ require 'spec_helper'
 
 kibana_version = '4.5'
 
+describe file('/etc/apt/sources.list.d/'\
+              'packages_elastic_co_kibana_4_5_debian.list') do
+  it { should be_file }
+  its('mode') { should eq '644' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  repo_url = 'http://packages.elastic.co/kibana/4.5/debian'
+  its('content') { should include "deb #{repo_url}" }
+end
+
 describe package('kibana') do
   it { should be_installed }
+  its('version') { should >= kibana_version }
 end
 
 describe file('/opt/kibana') do
   it { should exist }
   it { should be_directory }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should eq '755' }
+
+  # Intentionally redundant test for backwards compatibility. Prior versions
+  # of the role set /opt/kibana to a symlink for tarball extraction.
+  it { should_not be_symlink }
 end
 
 describe file('/etc/systemd/system/kibana.service') do
@@ -21,7 +39,7 @@ describe file('/etc/systemd/system/kibana.service') do
 
   desired_service_config_lines = [
     'Environment=NODE_OPTIONS="--max-old-space-size=200"',
-    'User=www-data',
+    'User=kibana',
     'Environment=CONFIG_PATH=/opt/kibana/config/kibana.yml',
     'Environment=NODE_ENV=production'
   ]
